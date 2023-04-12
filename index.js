4a915f312c13222b73cce4f3d0bf6c3d529da221
Walid Palekar <palw@tutamail.com>
2023-04-13 01:34:07 +0530

    python integration

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var createError = require("http-errors");
 var express = require("express");
+const {spawn} = require('child_process');
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
 
 app.use("/", indexRouter); // urls starting with '/' will go here
 //app.use("/users", usersRouter); //urls starting with '/users' will go here
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,40 @@
 var express = require("express");
 var router = express.Router();
+const {spawn} = require('child_process');
 
 /* GET home page. */
 router.get("/", function (req, res) {
-  res.render("pages/home", { title: "DARK-HACK" });
+
+  var dataToSend;
+
+  // spawn new child process to call the python script
+  const python = spawn('python', ['script1.py']);
+
+  // collect data from script
+  python.stdout.on('data', function (data) {
+    console.log('Pipe data from python script ...');
+    dataToSend = data.toString();
+    console.log(`stdout: ${dataToSend}`);
+  });
+
+  // in close event we are sure that stream from child process is closed
+  python.on('close', (code) => {
+  console.log(`child process close all stdio with code ${code}`);
+  // send data to browser
+  res.render("pages/home", 
+    { title: "DARK-HACK",
+      test_string : dataToSend
+    });
+
+  });
+ 
 });
 
 /* GET form page. */
 router.get("/form", function (req, res) {
   res.render("pages/form", { title: "DARK-HACK" });
 });
+
+// 
 
 module.exports = router;
